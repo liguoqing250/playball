e84fd9b88289f3e1bd85a90e4010e5908604b5b9
ljj943140754 <943140754@qq.com>
2020-07-15 18:30:38 +0800

    订单数据

---
+++ playball-master/playball/playball-module-appapi/src/main/java/org/jeecg/modules/appapi/controller/PlayballOrdersController.java
@@ -0,0 +1,46 @@
+package org.jeecg.modules.appapi.controller;
+
+
+import org.jeecg.common.api.vo.Result;
+import org.jeecg.modules.appapi.entity.PlayballOrders;
+import org.jeecg.modules.appapi.mapper.PlayballOrdersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 予之文化传媒公司
+ * @since 2020-07-15
+ */
+@RestController
+@RequestMapping("/playballOrders")
+public class PlayballOrdersController {
+
+	@Autowired
+	private PlayballOrdersMapper mapper;
+	
+	/* APP 调用添加订单数据*/
+	@PostMapping("/addPlayballOrders")
+	public Result<JSONObject> addPlayballOrders(@RequestBody PlayballOrders ord){
+		//返回对象
+		Result<JSONObject> result = new Result<JSONObject>();
+		System.err.println("添加订单数据" + ord);
+		int insert = mapper.insert(ord);
+		if(insert>0){
+			result.setCode(200);
+		}else{
+			result.setCode(0);
+		}
+		return result;
+	}
+}
+
---
+++ playball-master/playball/playball-module-appapi/src/main/java/org/jeecg/modules/appapi/service/PlayballOrdersService.java
@@ -0,0 +1,16 @@
+package org.jeecg.modules.appapi.service;
+
+import org.jeecg.modules.appapi.entity.PlayballOrders;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author 予之文化传媒公司
+ * @since 2020-07-15
+ */
+public interface PlayballOrdersService extends IService<PlayballOrders> {
+
+}
